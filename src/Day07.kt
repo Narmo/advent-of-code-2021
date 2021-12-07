df37eac40b991b19e2f1993b0036a81b2423e044
Nik Dyonin <nik@brite-apps.com>
2021-12-07 21:22:02 +0300

    added (commented out) frames rendering for day 7 part 1

--- src/Day07.kt
+++ src/Day07.kt
@@ -17,8 +17,64 @@ fun main() {
 	fun fuelConsumption(steps: Int): Int = steps * (steps + 1) / 2
 
 	fun part1(input: List<String>): Int {
-		val positions = getPositions(input[0])
-		val median = positions.median()
+		val positions = getPositions(input[0]).toMutableList()
+		val median = positions.toIntArray().median()
+
+//		val width = positions.maxOrNull()!!
+//		val height = positions.size
+//
+//		FileOutputStream(File("the-treachery-of-whales-part1.csv")).use { fos ->
+//			fos.bufferedWriter().use {
+//				val iterPositions = positions.toMutableList()
+//				var step = 0
+//
+//				it.write("step,${(0 until iterPositions.size).joinToString(",")}\n")
+//
+//				while (true) {
+//					val bi = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+//					val ig2 = bi.createGraphics()
+//					ig2.paint = Color.black
+//					ig2.drawRect(0, 0, width, height)
+//					ig2.paint = Color.yellow
+//
+//					var medianCounter = 0
+//
+//					it.write(step.toString())
+//
+//					for (i in iterPositions.indices) {
+//						it.write(",")
+//
+//						val curValue = iterPositions[i]
+//
+//						if (curValue > median) {
+//							iterPositions[i] = curValue - 1
+//						}
+//						else if (curValue < median) {
+//							iterPositions[i] = curValue + 1
+//						}
+//						else {
+//							medianCounter += 1
+//						}
+//
+//						it.write(iterPositions[i].toString())
+//
+//						ig2.drawRect(iterPositions[i], i, 1, 1)
+//					}
+//
+//					ImageIO.write(bi, "PNG", File("images/images${"%05d".format(step)}.png"))
+//
+//					ig2.dispose()
+//
+//					it.write("\n")
+//
+//					step += 1
+//
+//					if (medianCounter == iterPositions.size) {
+//						break
+//					}
+//				}
+//			}
+//		}
 
 		return positions.fold(0) { acc, i ->
 			acc + abs(i - median)
